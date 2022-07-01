a3b4476eff331cbf00fc23b9cf05e95a46b98975
Botasun <96219880+botasun@users.noreply.github.com>
2022-06-30 20:57:29 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "jenkins-terraform-evolvecybertraining"
+        bucket = "terraform-tfstate-099672477597"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
